70c8c18951bacd5c7a3eae31efe6586102a50413
sergey <sergey.zatsialepin@gmail.com>
2023-12-10 17:09:49 +0300

    add component button

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
+import { useState } from "react";
 import { ProjectSidebar } from "./components/ProjectSidebar";
-import './App.css';
 import { NewProject } from "./components/NewProject";
+import { NoProjectSelected } from "./components/NoProjectSelected";
+import './App.css';
 
 const App = () => {
+  const [projectState, setProjectState] = useState({
+    selectedProjectId: undefined,
+    projects: [],
+  });
+
+  const startAddProjectHandler = () => {
+    setProjectState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: null
+      };
+    });
+  }
+
+  let content;
+  if (projectState.selectedProjectId === null) {
+    content = <NewProject />;
+  } else if (projectState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={startAddProjectHandler}/>;
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectSidebar />
-      <NewProject />
+      <ProjectSidebar onStartAddProject={startAddProjectHandler}/>
+      {content}
     </main>
   );
 }
